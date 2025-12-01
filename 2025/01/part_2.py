4f30d3baddab2643d736c2d0ecb4067879035579
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-12-01 09:46:34 +0000

    feat: 2025 01 part 2

---
+++ 2025/01/part_2.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+def parse_line(line: str) -> int:
+    direction_key = {"L": -1, "R": 1}
+    direction = direction_key[line[0]]
+    magnitude = int(line[1:])
+    return direction * magnitude
+
+
+input_text = Path("2025/01/input.txt").read_text()
+rotations = [parse_line(line) for line in input_text.split("\n")]
+
+position = 50
+zero_count = 0
+
+for rotation in rotations:
+    new_position = position + rotation
+    position = new_position % 100
+    zero_count += abs(new_position // 100)
+
+print(zero_count)
