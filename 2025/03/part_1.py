8d6c96c32257c7321a25521b13a32097df7fc5c8
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-12-03 07:33:59 +0000

    feat: 2025 03 part 1

---
+++ 2025/03/part_1.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+input_text = Path("2025/03/input.txt").read_text()
+
+banks = input_text.split("\n")
+
+total_max_joltage = 0
+for bank in banks:
+    max_leading_digit = int(bank[0])
+    leading_digit_index = 0
+    for i, battery in enumerate(bank[:-1]):
+        joltage = int(battery)
+        if joltage > max_leading_digit:
+            max_leading_digit = joltage
+            leading_digit_index = i
+
+    max_trailing_digit = int(max(bank[leading_digit_index + 1 :]))
+
+    total_max_joltage += 10 * max_leading_digit + max_trailing_digit
+
+print(total_max_joltage)
