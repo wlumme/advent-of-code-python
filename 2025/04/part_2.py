fd50798a10617227a1671ccb2fb3bf95f889f1cd
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-12-04 09:32:24 +0000

    2025 04 part 2

---
+++ 2025/04/part_2.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+
+
+def print_grid(grid: dict[tuple[int, int], str]) -> None:
+    max_x = 0
+    max_y = 0
+    for x, y in grid:
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
+
+    rows = [[" " for _ in range(max_x + 1)] for __ in range(max_y + 1)]
+
+    for (x, y), char in grid.items():
+        rows[y][x] = char
+
+    pretty = "\n".join("".join(row) for row in rows)
+    print(pretty)
+
+
+def solve(input_text: str) -> int:
+    grid = {
+        (x, y): char
+        for y, row in enumerate(input_text.split("\n"))
+        for x, char in enumerate(row)
+    }
+
+    adjacent_offsets = [
+        (dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if dx != 0 or dy != 0
+    ]
+
+    roll_removed_on_last_pass = True
+    rolls_removed = 0
+    while roll_removed_on_last_pass:
+        roll_removed_on_last_pass = False
+        for (x, y), char in grid.items():
+            if char != "@":
+                continue
+
+            adjacent_roll_count = 0
+            for dx, dy in adjacent_offsets:
+                adjacent_xy = (x + dx, y + dy)
+
+                if adjacent_xy not in grid:
+                    continue
+
+                if grid[adjacent_xy] == "@":
+                    adjacent_roll_count += 1
+
+            if adjacent_roll_count < 4:
+                grid[(x, y)] = "X"
+                rolls_removed += 1
+                roll_removed_on_last_pass = True
+
+    return rolls_removed
+
+
+if __name__ == "__main__":
+    input_text = Path("2025/04/input.txt").read_text()
+    answer = solve(input_text)
+    print(answer)
